4ae23f05d491bb744d2f9e7cdb176e19cf459ddf
Sophie Ye <sophye@g.ucla.edu>
2025-05-13 22:04:39 -0700

    misunderstood how C works lol

--- update_weights.py
+++ update_weights.py
@@ -1,23 +1,20 @@
 """Compute dual ascent update for a given constraint
 
 Args: 
-    lamb: lambda (Lagrange multiplier)
-    rho: penalty weight
-    C: matrix associated with constraint (function?)
-    X: states
-    U: control inputs
-    ineq (bool): whether given constraint is an inequality constraint
-    gamma: how to update penalty weights; gamma > 1
+    lamb: lambda (Lagrange multiplier) vector
+    rho: penalty weight vector
+    C: constraints (values precomputed)
+    k_nci: number of inequality constraints
 
 Returns:
-    lamb: updated Lagrangian multiplier
+    lamb: updated Lagrangian multiplier vector
 """
-def dual_ascent_update(lamb, rho, C, X, U, ineq:bool, gamma):
-    if ineq:
-        lamb = max(0, lamb+rho*C(X,U))
-    else:
-        lamb+rho*C(X,U)
-    rho = gamma*rho
+def dual_ascent_update(lamb, rho, C, k_nci, gamma):
+    lamb = lamb + rho*C
+
+    # ineq constraints: lambda = max(0, lambda + rho*C)
+    for i in range(k_nci):
+        lamb[i] = max(0, lamb)
 
     return lamb
 

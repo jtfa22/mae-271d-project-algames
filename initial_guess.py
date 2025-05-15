45c0ef9cc7e73aa397055d5907e9da05cd35d7dd
edenzafran <zafraneden@gmail.com>
2025-05-15 09:52:54 -0700

    hstack syntax

--- initial_guess.py
+++ initial_guess.py
@@ -12,15 +12,15 @@ def generate(x0, M, N, n, m, dt):
     # integrate separately for each player, assuming
     # zero input as initial guess and all players have
     # identical dynamics
-    A = get_linear_dynamics(n,m,dt)[0]
+    A = get_linear_dynamics( n, m, dt)[0]
     for i in range(M):
         x = x0[i]
         for j in range(N):
             x = A@x
-            X = np.hstack(X, x)
+            X = np.hstack((X, x))
             
     U = np.zeros((M*N*m,)) # guess zero input
     mu = np.ones((M*N*n,)) # guess multipliers of 1
 
-    y0 = np.hstack(X,U,mu)
+    y0 = np.hstack((X,U,mu))
     return y0, X, U, mu

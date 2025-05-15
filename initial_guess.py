c2d9dcb997657a940df10c46e5de30e286b8de7a
Sophie Ye <sophye@g.ucla.edu>
2025-05-15 15:08:59 -0700

    fix initial state hstack bug

--- initial_guess.py
+++ initial_guess.py
@@ -12,12 +12,13 @@ def generate(x0, M, N, n, m, dt):
     # integrate separately for each player, assuming
     # zero input as initial guess and all players have
     # identical dynamics
+    X = np.empty((0,))
     A = get_linear_dynamics(n,m,dt)[0]
     for i in range(M):
         x = x0[i]
         for j in range(N):
             x = A@x
-            X = np.hstack(X, x)
+            X = np.hstack([X, x])
             
     U = np.zeros((M*N*m,)) # guess zero input
     mu = np.ones((M*N*n,)) # guess multipliers of 1

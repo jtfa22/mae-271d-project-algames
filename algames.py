bec72ca5f78ce0735c509c5ba02a4981543ccc77
edenzafran <zafraneden@gmail.com>
2025-05-15 16:12:37 -0700

    fix lambda[i] and y-yprev

--- algames.py
+++ algames.py
@@ -55,7 +55,7 @@ def ALGAMES(
     # ALGAMES loop - until y converge
     y = y0
     yprev = y + 2*eps*np.ones(y.shape)
-    while abs(yprev - y).any() > eps:  # TODO
+    while (abs(yprev - y) > eps).any() :  # TODO
         # solve G
         al_args = (
             M,

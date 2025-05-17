compute hessian for root solver, fixed convergence bug

algames.py
@@ -55,7 +55,8 @@ def ALGAMES(
     C = constraints.C(
         X_guess, U_guess, C_wall_sys, D_wall_sys, F_sys, G_sys, r, list_cola
     )
-    lam = np.ones((len(C),))*0.5   # start with lighter penalty weights
+
+    lam = np.zeros(len(C))
 
     # ALGAMES loop - until y converge
     y = y0
@@ -90,19 +91,24 @@ def ALGAMES(
 
         yprev = y
 
-        # currently uses derivative free method
-        # TODO derive the 2nd derivative of Lagrangian (if we have time)
+        # use solver
         sol = optimize.root(
-            aug_lagrangian.grad_aug_lagrangian, y, method="lm", jac=False, args=al_args
+            aug_lagrangian.grad_aug_lagrangian,
+            y,
+            method="lm",
+            jac=aug_lagrangian.hess_aug_lagrangian,
+            args=al_args,
         )
         y = sol.x
-        print(np.linalg.norm(y, np.inf))
 
         # split y into X, U, mu
         X, U, mu = np.split(y, [M * N * n, M * N * (n + m)])
 
         #  calculate new constraints
         C = constraints.C(X, U, C_wall_sys, D_wall_sys, F_sys, G_sys, r, list_cola)
+
+        # debug print
+        print(f"{iter} y_max={round(max(y),3)} rho={rho} C_vio={round(max(C),3)}")
 
         # dual ascent penalty update
         # current implementation only has ineq constraints

aug_lagrangian.py
@@ -5,49 +5,8 @@ import constraints
 import dynamics
 import numpy as np
 import objective
-
-
-# penalty matrix
-def Irho(C, lam, rho):
-    """Form penalty diagonal matrix"""
-    irho_vec = [rho if C[k] < 0 and lam[k] == 0 else 0 for k in range(len(C))]
-    return np.diag(irho_vec)
-
-
-def grad_penalty(X, U, Irho, C_wall_sys, D_wall_sys, F_sys, G_sys, r, list_cola):
-    """gradient of 1/2 * C.T @ Irho @ C wrt X, U"""
-    irho_vec = np.diag(Irho)
-
-    # wall
-    size_wall = np.shape(C_wall_sys)[0]
-    Irho1 = np.diag(irho_vec[:size_wall])
-    c_wall_x = (C_wall_sys @ X - D_wall_sys).T @ Irho1 @ C_wall_sys
-    c_wall_u = np.zeros(len(U))
-    c_wall = np.hstack((c_wall_x, c_wall_u))
-
-    # input
-    size_input = np.shape(F_sys)[0]
-    Irho2 = np.diag(irho_vec[size_wall : size_wall + size_input])
-    c_input_x = np.zeros(len(X))
-    c_input_u = (F_sys @ U - G_sys).T @ Irho2 @ F_sys
-    c_input = np.hstack((c_input_x, c_input_u))
-
-    # cola
-    size_cola = len(list_cola)
-    assert size_wall + size_input + size_cola == len(irho_vec)
-    irho3_vec = irho_vec[size_wall + size_input :]
-    c_cola_x = np.sum(
-        [
-            2 * -X.T @ C_k.T @ C_k * rho_k * (r - (C_k @ X).T @ (C_k @ X))
-            for C_k, rho_k in zip(list_cola, irho3_vec)
-        ],
-        axis=0,
-    )
-    c_cola_u = np.zeros(len(U))
-    c_cola = np.hstack((c_cola_x, c_cola_u))
-
-    # add all
-    return c_wall + c_input + c_cola
+import penalty
+from scipy import linalg
 
 
 # augmented lagrangian
@@ -76,6 +35,15 @@ def grad_L_v(u_v, N, n, m, mu, lam, grad_J_v, grad_D, grad_C, grad_penalty):
     ]
 
     return grad_J_v + mu_v.T @ grad_D_v + lam.T @ grad_C + grad_penalty
+
+
+def hess_L_v(u_v, N, n, m, mu, lam, hess_J_v, hess_D, hess_C, hess_penalty):
+    """hessian of Augmented Lagrangian of one player wrt X, U"""
+    # ignore correct selection since hess_D is all zero
+    mu_v = mu
+    hess_D_v = hess_D
+
+    return hess_J_v + mu_v.T @ hess_D_v + lam.T @ hess_C + hess_penalty
 
 
 def grad_aug_lagrangian(
@@ -110,7 +78,7 @@ def grad_aug_lagrangian(
     C = constraints.C(X, U, C_wall_sys, D_wall_sys, F_sys, G_sys, r, list_cola)
 
     # compute new penalty matrix
-    I_rho = Irho(C, lam, rho)
+    I_rho = penalty.Irho(C, lam, rho)
 
     # compute derivative of augmented lagrangian
     list_grad_J_v = [
@@ -120,7 +88,7 @@ def grad_aug_lagrangian(
     grad_C = constraints.grad_C(
         X, U, C_wall_sys, D_wall_sys, F_sys, G_sys, r, list_cola
     )
-    grad_C_penalty = grad_penalty(
+    grad_C_penalty = penalty.grad_penalty(
         X, U, I_rho, C_wall_sys, D_wall_sys, F_sys, G_sys, r, list_cola
     )
 
@@ -134,3 +102,64 @@ def grad_aug_lagrangian(
     g_players.append(dynamics.D(X, U, A_sys, B_sys, E_sys, x0) * dynamics_mult)
 
     return np.concatenate(g_players)
+
+
+def hess_aug_lagrangian(
+    y,
+    M,
+    N,
+    n,
+    m,
+    lam,
+    rho,
+    Q,
+    Qf,
+    R,
+    list_x0,
+    list_xf,
+    A_sys,
+    B_sys,
+    E_sys,
+    C_wall_sys,
+    D_wall_sys,
+    F_sys,
+    G_sys,
+    r,
+    list_cola,
+    dynamics_mult,
+):
+    """hessian of Augmented Lagrangian of all players wrt X, U"""
+    # split y into X, U, mu
+    X, U, mu = np.split(y, [M * N * n, M * N * (n + m)])
+
+    # calculate current constraints
+    C = constraints.C(X, U, C_wall_sys, D_wall_sys, F_sys, G_sys, r, list_cola)
+
+    # compute new penalty matrix
+    I_rho = penalty.Irho(C, lam, rho)
+
+    # compute derivative of augmented lagrangian
+    list_hess_J_v = [
+        objective.hess_J_v(X, U, mu, u_v, Q, Qf, R, M, N, n, m, list_xf)
+        for u_v in range(M)
+    ]
+    hess_D = dynamics.hess_D(X, U, mu, A_sys, B_sys, E_sys)
+    hess_C = constraints.hess_C(
+        X, U, mu, C_wall_sys, D_wall_sys, F_sys, G_sys, r, list_cola
+    )
+    hess_C_penalty = penalty.hess_penalty(
+        X, U, mu, I_rho, C_wall_sys, D_wall_sys, F_sys, G_sys, r, list_cola
+    )
+
+    # compte hessian of aug lagrangian for each player
+    g_players = [
+        hess_L_v(u_v, N, n, m, mu, lam, hess_J_v, hess_D, hess_C, hess_C_penalty)
+        for u_v, hess_J_v in enumerate(list_hess_J_v)
+    ]
+    # append dynamics to root solving problem
+    x0 = np.hstack(list_x0)
+    grad_D = dynamics.grad_D(X, U, A_sys, B_sys, E_sys) * dynamics_mult
+    grad_D = np.hstack((grad_D, np.zeros((len(X), len(mu)))))
+    g_players.append(grad_D)
+
+    return np.vstack(g_players)

constraints.py
@@ -41,6 +41,16 @@ def grad_C_wall(X, U, C_wall_sys, D_wall_sys):
     return np.hstack((C_wall_sys, np.zeros((np.shape(C_wall_sys)[0], len(U)))))
 
 
+def hess_C_wall(X, U, mu, C_wall_sys, D_wall_sys):
+    """hessian wrt X, U, mu"""
+    len_xu = len(X) + len(U)
+    grad_C_wall = np.hstack(
+        (C_wall_sys, np.zeros((np.shape(C_wall_sys)[0], len(U))), C_wall_sys)
+    )
+    matrix = np.zeros(np.shape(grad_C_wall))
+    return np.array([matrix] * len_xu)
+
+
 # control input inequality constraint
 def get_linear_input_bound(m, max_x=5, max_y=5):
     """Control inequality for a single player at one timestep: f_ineq * x_k - g_ineq <= 0"""
@@ -81,10 +91,18 @@ def grad_C_input(X, U, F_sys, G_sys):
     return np.hstack((np.zeros((np.shape(F_sys)[0], len(X))), F_sys))
 
 
+def hess_C_input(X, U, mu, F_sys, G_sys):
+    """hessian wrt X, U, mu"""
+    len_xu = len(X) + len(U)
+    grad_C_input = np.hstack((np.zeros((np.shape(F_sys)[0], len(X) * 2)), F_sys))
+    matrix = np.zeros(np.shape(grad_C_input))
+    return np.array([matrix] * len_xu)
+
+
 # collision avoidance inequality constraint
 def get_system_cola(M, N, n):
     """Collision avoidance inequality for all players:
-    r - (C_cola_k_v1_v2 * X).T @ (C_cola_k_v1_v2 * X) <= 0
+    r^2 - (C_cola_k_v1_v2 * X).T @ (C_cola_k_v1_v2 * X) <= 0
     each C_cola matrix is formulated per timestep k, per players v1, v2"""
     pos = np.hstack(
         (np.eye(2), np.zeros((2, 2)))
@@ -106,7 +124,7 @@ def get_system_cola(M, N, n):
 
 def C_cola(X, U, r, list_cola):
     """collision avoidance inequality constraint:
-    r - (C_cola_k_v1_v2 @ X).T @ (C_cola_k_v1_v2 @ X) <= 0"""
+    r^2 - (C_cola_k_v1_v2 @ X).T @ (C_cola_k_v1_v2 @ X) <= 0"""
     C_k_v1_v2 = [r**2 - (C_k @ X).T @ (C_k @ X) for C_k in list_cola]
     return np.array(C_k_v1_v2)
 
@@ -116,6 +134,30 @@ def grad_C_cola(X, U, r, list_cola):
     C_x = np.vstack([-X.T @ C_k.T @ C_k for C_k in list_cola])
     C_u = np.zeros((np.shape(C_x)[0], len(U)))
     return np.hstack((C_x, C_u))
+
+
+def hess_C_cola(X, U, mu, r, list_cola):
+    """hessian wrt X, U, mu"""
+    len_xu = len(X) + len(U)
+    len_y = len(X) + len(U) + len(mu)
+    C_xu = np.dstack(
+        [
+            np.vstack(
+                (
+                    np.hstack(
+                        (
+                            -C_k.T @ C_k,
+                            np.zeros((len(X), len(U))),
+                            np.zeros((len(X), len(mu))),
+                        )
+                    ),
+                    np.zeros((len(U), len_y)),
+                )
+            )
+            for C_k in list_cola
+        ]
+    )
+    return np.reshape(C_xu, (len_xu, -1, len_y))
 
 
 # all constraints combined
@@ -133,3 +175,11 @@ def grad_C(X, U, C_wall_sys, D_wall_sys, F_sys, G_sys, r, list_cola):
     c_input = grad_C_input(X, U, F_sys, G_sys)
     c_cola = grad_C_cola(X, U, r, list_cola)
     return np.vstack((c_wall, c_input, c_cola))
+
+
+def hess_C(X, U, mu, C_wall_sys, D_wall_sys, F_sys, G_sys, r, list_cola):
+    """Hessian of constraints inequality of system (matrix)"""
+    c_wall = hess_C_wall(X, U, mu, C_wall_sys, D_wall_sys)
+    c_input = hess_C_input(X, U, mu, F_sys, G_sys)
+    c_cola = hess_C_cola(X, U, mu, r, list_cola)
+    return np.concatenate((c_wall, c_input, c_cola), axis=1)